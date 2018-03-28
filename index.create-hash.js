a99bf3866d73717e07c7e2301aba25bbbb14e35a
Ian McGregor <ian@mcgregor.codes>
2018-03-28 17:08:58 +0100

    :wrench: Add create hash

---
+++ index.create-hash.js
@@ -0,0 +1,10 @@
+// const Buffer = require('buffer').Buffer;
+// module.exports = require('crypto');
+// module.exports = {
+// createHash: require('crypto').createHash,
+// createHash: require('create-hash/browser')
+// randomBytes: require('react-native-random-bytes')
+// randomBytes: () => {}
+// };
+
+module.exports = require('create-hash/browser');
--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
 import nav from './nav';
+// import crypto from './crypto';
+import createHash from './create-hash';
+// const randomBytes = require('react-native-randombytes');
+
+// const crypto = {
+//     createHash,
+//     randomBytes
+// };
+//
+// console.log('crypto', Object.keys(crypto).join(','));
+// console.log('crypto.createHash', crypto.createHash('sha256').update('Tx hash').digest());
+console.log('crypto.createHash', createHash('sha256').update('Tx hash').digest());
+// console.log('crypto.randomBytes', crypto.randomBytes(64));
+// console.log('crypto.randomBytes', randomBytes(64));
 
 export default nav;
