ea6388cf344f6f99fe82f4466ac948b4c0eebe99
Ian McGregor <ian@mcgregor.codes>
2018-04-26 17:14:42 +0100

    :cd: Update storage util to take different keys

--- src/app/actions/profile.js
+++ src/app/actions/profile.js
@@ -1,6 +1,8 @@
 import {load, save, clear} from '../utils/storage';
 import wait from './wait';
 
+const storageKey = 'profile';
+
 export const PROFILE_LOADING = 'PROFILE_LOADING';
 export const PROFILE_UPDATE = 'PROFILE_UPDATE';
 export const PROFILE_AVAILABLE = 'PROFILE_AVAILABLE';
@@ -10,7 +12,7 @@ export const profileLoading = value => ({type: PROFILE_LOADING, value});
 
 export const profileLoad = () => dispatch => {
     dispatch(profileLoading(true));
-    return load()
+    return load(storageKey)
         .then(data => wait(0.5, data))
         .then(data => {
             dispatch({type: PROFILE_UPDATE, ...data});
@@ -21,12 +23,12 @@ export const profileLoad = () => dispatch => {
 
 export const profileUpdate = data => dispatch => {
     dispatch({type: PROFILE_UPDATE, ...data});
-    return save(data);
+    return save(storageKey, data);
 };
 
 export const profileAvailable = value => ({type: PROFILE_AVAILABLE, value});
 
 export const profileClear = () => dispatch => {
     dispatch(profileAvailable(false));
-    return clear();
+    return clear(storageKey);
 };
