e91e2017ce34d124ce99337788393d8b2aaef613
Ian McGregor <ian@mcgregor.codes>
2018-05-11 13:37:48 +0100

    :video_game: Add Tiled src files

---
+++ tiled/pigzbe_game_tileset_01.tsx
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="pigzbe_game_tileset_01" tilewidth="64" tileheight="64" tilecount="361" columns="19">
+ <image source="assets/tilesets/pigzbe_game_tileset_01.png" width="1216" height="1216"/>
+ <tile id="96">
+  <animation>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="57" duration="100"/>
+   <frame tileid="76" duration="100"/>
+   <frame tileid="95" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="58" duration="100"/>
+   <frame tileid="77" duration="100"/>
+   <frame tileid="96" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="98">
+  <animation>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="59" duration="100"/>
+   <frame tileid="78" duration="100"/>
+   <frame tileid="97" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="79" duration="100"/>
+   <frame tileid="98" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="100">
+  <animation>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="80" duration="100"/>
+   <frame tileid="99" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="62" duration="100"/>
+   <frame tileid="81" duration="100"/>
+   <frame tileid="100" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="110">
+  <animation>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="88" duration="100"/>
+   <frame tileid="107" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="108" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="71" duration="100"/>
+   <frame tileid="90" duration="100"/>
+   <frame tileid="109" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="72" duration="100"/>
+   <frame tileid="91" duration="100"/>
+   <frame tileid="110" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="112">
+  <animation>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="73" duration="100"/>
+   <frame tileid="92" duration="100"/>
+   <frame tileid="111" duration="100"/>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="55" duration="100"/>
+   <frame tileid="74" duration="100"/>
+   <frame tileid="93" duration="100"/>
+   <frame tileid="112" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
