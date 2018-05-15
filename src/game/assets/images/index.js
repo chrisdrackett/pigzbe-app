4f23d95c0d1e01666e534f8de07fd6ec302b0cab
Ian McGregor <ian@mcgregor.codes>
2018-05-15 08:27:37 +0100

    :construction: Decompose and repack main tilemap

--- src/game/assets/images/index.js
+++ src/game/assets/images/index.js
@@ -3,10 +3,11 @@ export default {
     arrow: require('./arrow.png'),
     fontPng: require('../fonts/font.png'),
 
-    pigzbe_game_tileset_01: require('./textures/pigzbe_game_tileset_01.png'),
+    // pigzbe_game_tileset_01: require('./textures/pigzbe_game_tileset_01.png'),
     clouds_01: require('./textures/clouds_01.png'),
     clouds_02: require('./textures/clouds_02.png'),
     hills_01: require('./textures/hills_01.png'),
     mountains_01: require('./textures/mountains_01.png'),
     objects0: require('./textures/objects0.png'),
+    tiles0: require('./textures/tiles0.png'),
 };
