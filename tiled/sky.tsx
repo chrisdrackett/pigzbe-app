e0710432c9b02e344fd8d8819f2b6d37369f19a2
Ian McGregor <ian@mcgregor.codes>
2018-05-16 16:30:47 +0100

    :art: Group tilemap layers and make bgs full width

--- tiled/sky.tsx
+++ tiled/sky.tsx
@@ -1,20 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="sky" tilewidth="3315" tileheight="960" tilecount="6" columns="0">
+<tileset name="sky" tilewidth="3328" tileheight="960" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="3315" height="545" source="assets/backgrounds/clouds_01.png"/>
+  <image width="3328" height="545" source="assets/backgrounds/clouds_01.png"/>
  </tile>
  <tile id="1">
-  <image width="3315" height="545" source="assets/backgrounds/clouds_02.png"/>
+  <image width="3328" height="545" source="assets/backgrounds/clouds_02.png"/>
  </tile>
  <tile id="2">
-  <image width="3315" height="545" source="assets/backgrounds/hills_01.png"/>
+  <image width="1664" height="545" source="assets/backgrounds/hills_01.png"/>
  </tile>
  <tile id="3">
-  <image width="3315" height="545" source="assets/backgrounds/mountains_01.png"/>
+  <image width="3328" height="545" source="assets/backgrounds/mountains_01.png"/>
  </tile>
  <tile id="4">
-  <image width="3315" height="545" source="assets/objects/sun_01.png"/>
+  <image width="3328" height="545" source="assets/objects/sun_01.png"/>
  </tile>
  <tile id="5">
   <image width="128" height="960" source="assets/objects/sky.png"/>
