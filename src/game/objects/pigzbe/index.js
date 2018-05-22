ef3bac138ad2bf559da568cf4d9726290778c5df
Ian McGregor <ian@mcgregor.codes>
2018-05-22 10:24:28 +0100

    :pig: Add pigzbe character layers and movement offsets

--- src/game/objects/pigzbe/index.js
+++ src/game/objects/pigzbe/index.js
@@ -1,6 +1,7 @@
+import lerp from 'usfl/math/lerp';
 import {Container, Sprite} from 'pixi.js';
 const {
-    round,
+    abs,
     min,
     max
 } = Math;
@@ -9,19 +10,44 @@ export default class Pigzbe {
     constructor({x, y, maxX, maxY, mapW}) {
         this.x = x;
         this.y = y;
+        this.dx = 0;
+        this.dy = 0;
+        this.ox = 0;
+        this.oy = 0;
         this.maxX = maxX;
         this.maxY = maxY;
         this.mapW = mapW;
 
         this.sprite = new Container();
-        const pig = Sprite.from('pig');
-        pig.position.set(0 - round(pig.width / 2), 0 - round(pig.height / 2));
-        this.sprite.addChild(pig);
         this.sprite.position.set(x, y);
-        this.w = pig.width;
-        this.h = pig.height;
+        this.w = 128;
+        this.h = 128;
         this.hw = this.w / 2;
         this.hh = this.h / 2;
+
+        const container = new Container();
+        this.sprite.addChild(container);
+        container.position.set(0 - this.hw, 0 - this.hh);
+
+        const feet = Sprite.from('pigzbe_feet');
+        container.addChild(feet);
+
+        const bg = Sprite.from('pigzbe_bodybackground');
+        container.addChild(bg);
+
+        const body = Sprite.from('pigzbe_body');
+        container.addChild(body);
+
+        const eyes = Sprite.from('pigzbe_eyes');
+        container.addChild(eyes);
+
+        const nose = Sprite.from('pigzbe_nose');
+        container.addChild(nose);
+
+        this.feet = feet;
+        this.bg = bg;
+        this.eyes = eyes;
+        this.nose = nose;
 
         this.hitRect = {
             top: 0,
@@ -32,8 +58,10 @@ export default class Pigzbe {
     }
 
     update(vec, delta) {
-        this.x += vec.x * delta;
-        this.y += vec.y * delta;
+        this.dx = vec.x * delta;
+        this.dy = vec.y * delta;
+        this.x += this.dx;
+        this.y += this.dy;
 
         if (this.maxX) {
             this.x = max(this.hw, min(this.x, this.maxX - this.hw));
@@ -58,5 +86,31 @@ export default class Pigzbe {
         this.hitRect.bottom = this.y + 20;
         this.hitRect.left = this.rx - 30;
         this.hitRect.right = this.rx + 30;
+
+        const tx = max(-4, min(this.dx, 4));
+        const ty = max(-4, min(this.dy, 4));
+
+        this.ox = lerp(this.ox, 0 - tx, 0.2);
+        this.oy = lerp(this.oy, 0 - ty, 0.2);
+
+        if (abs(this.ox) < 0.01) {
+            this.ox = 0;
+        }
+
+        if (abs(this.oy) < 0.01) {
+            this.oy = 0;
+        }
+
+        this.bg.position.x = this.ox;
+        this.bg.position.y = this.oy;
+
+        this.feet.position.x = this.ox * -0.5;
+        this.feet.position.y = this.oy * 0.5;
+
+        this.eyes.position.x = this.ox * -0.5;
+        this.eyes.position.y = this.oy * -0.5;
+
+        this.nose.position.x = this.ox * -1;
+        this.nose.position.y = this.oy * -1;
     }
 }
