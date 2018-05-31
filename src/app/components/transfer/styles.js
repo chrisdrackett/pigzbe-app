6e4a49ceccbce43b6990202daf644e62c4bbc1ab
Ian McGregor <ian@mcgregor.codes>
2018-05-31 13:04:59 +0100

    :nail_care: Clean up transfer view

--- src/app/components/transfer/styles.js
+++ src/app/components/transfer/styles.js
@@ -2,7 +2,6 @@ import {StyleSheet} from 'react-native';
 import {
     container,
     color,
-    fontFamily,
     paddingH
 } from '../../styles';
 
@@ -10,14 +9,6 @@ export default StyleSheet.create({
     container,
     pig: {
         marginTop: 20,
-    },
-    containerBody: {
-        backgroundColor: color.red,
-        width: '100%',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'stretch',
     },
     buttonWrapper: {
         backgroundColor: color.lightGrey,
@@ -32,33 +23,5 @@ export default StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         alignSelf: 'stretch',
-    },
-
-    transaction: {
-        paddingLeft: 20,
-        paddingRight: 20,
-        paddingTop: 20,
-        paddingBottom: 20,
-        borderBottomWidth: 1,
-        borderBottomColor: color.mediumGrey
-    },
-    date: {
-        fontFamily,
-        color: color.blue,
-        fontWeight: 'bold',
-        fontSize: 14,
-        marginBottom: 5
-    },
-    text: {
-        fontFamily,
-        color: color.darkGrey,
-        fontSize: 14
-    },
-    address: {
-        fontFamily,
-        color: color.darkGrey,
-        fontSize: 10,
-        maxWidth: '100%',
-        overflow: 'hidden',
     },
 });
