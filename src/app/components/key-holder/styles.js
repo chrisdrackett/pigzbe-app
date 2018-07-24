0112dfe3b9cb76cafde59c0fef74447f90274be7
Ian McGregor <ian@mcgregor.codes>
2018-07-24 14:19:20 +0100

    :art: Design updates

--- src/app/components/key-holder/styles.js
+++ src/app/components/key-holder/styles.js
@@ -6,50 +6,26 @@ import {
 } from '../../styles';
 
 export default StyleSheet.create({
-    boxKeys: {
-        borderRadius: 10,
+    box: {
+        backgroundColor: color.lightRed,
+        borderRadius: 5,
         borderWidth: 1,
-        borderColor: color.white,
-        paddingTop: 20,
-        paddingBottom: 10,
-        paddingLeft: 18,
-        paddingRight: 18,
+        borderColor: color.lightRed,
+        paddingTop: 18,
+        paddingBottom: 18,
+        paddingLeft: 20,
+        paddingRight: 20,
         marginTop: 20,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'stretch'
+        marginBottom: 20,
+        width: 300,
     },
-    boxKeysTitle: {
+    content: {
         fontFamily,
         color: color.white,
+        fontSize: 14,
         fontWeight: 'bold',
-        fontSize: 10,
-        textAlign: 'center',
-        backgroundColor: color.blue,
-        marginTop: -28,
-        alignSelf: 'center',
-        paddingLeft: 8,
-        paddingRight: 8,
     },
-    boxTx: {
-        backgroundColor: color.green
-    },
-    boxKeysInner: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 10,
-    },
-    boxKeysText: {
-        fontFamily,
-        color: color.white,
-        fontWeight: 'bold',
-        fontSize: 12,
-        textAlign: 'left',
-        width: 165,
-    },
-    boxKeysCopy: {
-        width: 45,
-        height: 45,
+    normal: {
+        fontWeight: 'normal',
     },
 });
