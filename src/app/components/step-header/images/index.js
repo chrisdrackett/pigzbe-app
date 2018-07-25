b4031a711046acf33b39b068ab1f5576f7fc7eff
Ian McGregor <ian@mcgregor.codes>
2018-07-25 12:53:04 +0100

    :construction: Apply new template to onboarding screens

--- src/app/components/step-header/images/index.js
+++ src/app/components/step-header/images/index.js
@@ -2,4 +2,6 @@ export default {
     tick: require('./tick.png'),
     code: require('./code.png'),
     'touch-id': require('./touch-id.png'),
+    device: require('./device.png'),
+    secure: require('./secure.png'),
 };
