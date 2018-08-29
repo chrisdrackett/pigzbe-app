2a5fd3702a387521ca99215510514904a7e01537
Dine Albrecht <hello@dinealbrecht.net>
2018-08-29 17:40:04 +0200

    remove obsolete console log

--- src/app/utils/image-picker/index.js
+++ src/app/utils/image-picker/index.js
@@ -18,10 +18,9 @@ const options = {
 };
 
 export const pickImage = () => {
-    console.log('pickImage');
     return new Promise((resolve, reject) => {
         ImagePicker.showImagePicker(options, (response) => {
-            console.log('Response = ', response);
+        // console.log('Response = ', response);
 
             if (response.didCancel) {
                 console.log('User cancelled image picker');
