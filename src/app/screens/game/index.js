afddf9c28782d4ad76d546fcb303feb6daea397a
Ian McGregor <ian@mcgregor.codes>
2018-09-26 08:38:28 +0100

    :fire: Remove dead code

--- src/app/screens/game/index.js
+++ src/app/screens/game/index.js
@@ -3,52 +3,9 @@ import {connect} from 'react-redux';
 import {View, Text} from 'react-native';
 import styles from './styles';
 import Learn from '../learn';
-import {gameWolloCollected, gameOverlayOpen} from '../../actions';
-import {PAUSE, RESUME, READY, COLLECTED, LEARN, LOG, ERROR} from '../../../game/constants';
 import GameTasks from '../game-tasks';
 
 export class Game extends Component {
-    state = {
-        message: 'Loading game',
-    }
-
-    onBlur = () => this.sendPostMessage(PAUSE)
-
-    onFocus = () => this.sendPostMessage(RESUME)
-
-    onError = error => console.log(error)
-
-    onMessage = event => {
-        const {dispatch} = this.props;
-        const message = event.nativeEvent.data;
-        const {name, value} = JSON.parse(message);
-
-        // console.log('On Message', name, value);
-        switch (name) {
-            case READY:
-                this.setState({loading: false});
-                break;
-            case COLLECTED:
-                dispatch(gameWolloCollected(value));
-                break;
-            case LEARN:
-                dispatch(gameOverlayOpen(true));
-                break;
-            case LOG:
-                console.log('webview log', value);
-                break;
-            case ERROR:
-                console.log('webview error', value);
-                break;
-            default:
-        }
-    }
-
-    sendPostMessage(msg) {
-        console.log('Sending post message', msg);
-        this.el.postMessage(msg);
-    }
-
     render() {
         const {
             dispatch,
