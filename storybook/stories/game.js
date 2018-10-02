bbfe522aea5fd3aba7b21adf97806739cb109440
Ian McGregor <ian@mcgregor.codes>
2018-10-02 14:17:18 +0100

    :books: Add kid actions to storybook

--- storybook/stories/game.js
+++ storybook/stories/game.js
@@ -34,7 +34,41 @@ const props = {
         dob: '01/01/2010',
         photo: null,
         balance: '20',
-        tasks: []
+        tasks: [],
+        actions: [
+            {
+                memo: 'Task: Tidy your room',
+                type: 'task',
+                amount: '7',
+                totalAmount: '7',
+                hash: '6d3c2a5960fc02cb9cc87a6f74d2c8ebc64a795e079589bb3a618185095ac866',
+                date: '2018-10-02T13:09:00Z'
+            },
+            {
+                memo: 'From dad',
+                type: 'gift',
+                amount: '14',
+                totalAmount: '14',
+                hash: '8dc2a1571d8e781398d67e26b6520dcd23f40eef259e126476441f02160333e6',
+                date: '2018-10-02T13:09:15Z'
+            },
+            {
+                memo: 'Allowance #2.1 to Iggy',
+                type: 'allowance',
+                amount: '2',
+                totalAmount: '2',
+                hash: 'bc4fc79e3ebb25a7a5cab899654abb262e69ba9b218676604151319a014c26de',
+                date: '2018-10-02T13:09:45Z'
+            },
+            {
+                memo: 'Allowance #2.1 to Iggy',
+                type: 'allowance',
+                amount: '2',
+                totalAmount: '2',
+                hash: '18e3fb908459e4dfe3d3a4493a9a4ed5fb62295d6eed3330093e7f2543c5e24d',
+                date: '2018-10-02T13:09:50Z'
+            }
+        ]
     },
     parentNickname: 'Dad',
 };
