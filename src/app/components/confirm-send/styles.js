6577a6015b93e6d62e895137ffe6d5729d847a2e
Jake Salmon <jnsalmon@gmail.com>
2018-10-19 14:03:42 +0100

    Update confirm modal styling

--- src/app/components/confirm-send/styles.js
+++ src/app/components/confirm-send/styles.js
@@ -1,6 +1,9 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
+    title: {
+        textAlign: 'center',
+    },
     bold: {
         fontWeight: 'bold',
     },
--- src/app/components/modal/index.js
+++ src/app/components/modal/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {View, Image} from 'react-native';
 import styles from './styles';
 
-export default ({children}) => (
+export default ({children, noBird=false}) => (
     <View style={styles.overlay}>
         <View>
-            <Image source={require('./images/bird.png')} style={styles.bird}/>
+            {!noBird &&
+                <Image source={require('./images/bird.png')} style={styles.bird}/>
+            }
             <View style={styles.container}>
                 {children}
             </View>
