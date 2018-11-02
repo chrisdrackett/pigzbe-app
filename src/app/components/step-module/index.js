9c814bf7b461cfc6c2a184f4a29f586d8f5f42eb
Jake Salmon <jnsalmon@gmail.com>
2018-11-02 11:05:48 +0000

    Fix indentation

--- src/app/components/step-module/index.js
+++ src/app/components/step-module/index.js
@@ -11,107 +11,107 @@ import {color} from '../../styles';
 
 export default class StepModule extends Component {
 
-  static defaultProps = {
-      scroll: true,
-      keyboardOffset: 0,
-      avoidKeyboard: true,
-  }
+    static defaultProps = {
+        scroll: true,
+        keyboardOffset: 0,
+        avoidKeyboard: true,
+    }
 
-  render() {
-      const {
-          title,
-          content,
-          icon,
-          error,
-          scroll,
-          children,
-          headerChildren,
-          backgroundColor,
-          onBack,
-          pad,
-          onSettings,
-          settingsIcon,
-          paddingTop,
-          keyboardOffset,
-          loading,
-          loaderMessage,
-          customTitle,
-          hideLogo,
-          justify,
-          avoidKeyboard,
-          keyboardAvoidPad
-      } = this.props;
+    render() {
+        const {
+            title,
+            content,
+            icon,
+            error,
+            scroll,
+            children,
+            headerChildren,
+            backgroundColor,
+            onBack,
+            pad,
+            onSettings,
+            settingsIcon,
+            paddingTop,
+            keyboardOffset,
+            loading,
+            loaderMessage,
+            customTitle,
+            hideLogo,
+            justify,
+            avoidKeyboard,
+            keyboardAvoidPad
+        } = this.props;
 
-      const {height} = Dimensions.get('window');
+        const {height} = Dimensions.get('window');
 
-      const container = (
-          <Container
-              scroll={scroll}
-              style={[styles.container, {
-                  backgroundColor: (!error && backgroundColor) || color.white,
-              }]}
-          >
-              {(content || error) && (
-                  <View style={[styles.containerText, {paddingTop: height > 568 ? 32 : 25}]}>
-                      {typeof content === 'string' ? (
-                          <Paragraph>{content}</Paragraph>
-                      ) : (
-                          content
-                      )}
-                      {error && (
-                          <Paragraph style={styles.error}>{(error.message || error)}</Paragraph>
-                      )}
-                  </View>
-              )}
-              <View
-                  style={[
-                      styles.wrapper,
-                      justify ? {justifyContent: justify} : null,
-                      pad ? styles.pad : null, paddingTop ? {paddingTop} : null
-                  ]}>
-                  {children}
-              </View>
-              <Loader
-                  light
-                  loading={loading}
-                  message={loaderMessage}
-                  style={{
-                      borderTopRightRadius: 5,
-                      borderTopLeftRadius: 5,
-                      backgroundColor: color.white,
-                  }}
-              />
-          </Container>
-      );
+        const container = (
+            <Container
+                scroll={scroll}
+                style={[styles.container, {
+                    backgroundColor: (!error && backgroundColor) || color.white,
+                }]}
+            >
+                {(content || error) && (
+                    <View style={[styles.containerText, { paddingTop: height > 568 ? 32 : 25 }]}>
+                        {typeof content === 'string' ? (
+                            <Paragraph>{content}</Paragraph>
+                        ) : (
+                                content
+                            )}
+                        {error && (
+                            <Paragraph style={styles.error}>{(error.message || error)}</Paragraph>
+                        )}
+                    </View>
+                )}
+                <View
+                    style={[
+                        styles.wrapper,
+                        justify ? { justifyContent: justify } : null,
+                        pad ? styles.pad : null, paddingTop ? { paddingTop } : null
+                    ]}>
+                    {children}
+                </View>
+                <Loader
+                    light
+                    loading={loading}
+                    message={loaderMessage}
+                    style={{
+                        borderTopRightRadius: 5,
+                        borderTopLeftRadius: 5,
+                        backgroundColor: color.white,
+                    }}
+                />
+            </Container>
+        );
 
-      return (
-          <SafeAreaView style={styles.safeArea}>
-              <Container style={styles.wrapper} scroll={false}>
-                  <View style={styles.bg}/>
-                  <Header
-                      onBack={onBack}
-                      onSettings={onSettings}
-                      settingsIcon={settingsIcon}
-                      hideLogo={hideLogo}
-                      customTitle={customTitle}
-                      loading={loading}
-                  />
-                  {
-                      title || icon || headerChildren ? <StepHeader title={title} icon={icon}>{headerChildren}</StepHeader> : null
-                  }
-                  {avoidKeyboard &&
-                <KeyboardAvoid style={{flex: 1}} containerStyle={{flexGrow: 1}} offset={keyboardOffset} pad={keyboardAvoidPad}>
-                    {container}
-                </KeyboardAvoid>
-                  }
-                  {!avoidKeyboard &&
-                container
-                  }
-              </Container>
-              <View style={styles.safeAreaBottom}>
-                  <View style={styles.safeAreaBottomInner} />
-              </View>
-          </SafeAreaView>
-      );
-  }
+        return (
+            <SafeAreaView style={styles.safeArea}>
+                <Container style={styles.wrapper} scroll={false}>
+                    <View style={styles.bg} />
+                    <Header
+                        onBack={onBack}
+                        onSettings={onSettings}
+                        settingsIcon={settingsIcon}
+                        hideLogo={hideLogo}
+                        customTitle={customTitle}
+                        loading={loading}
+                    />
+                    {
+                        title || icon || headerChildren ? <StepHeader title={title} icon={icon}>{headerChildren}</StepHeader> : null
+                    }
+                    {avoidKeyboard &&
+                        <KeyboardAvoid style={{ flex: 1 }} containerStyle={{ flexGrow: 1 }} offset={keyboardOffset} pad={keyboardAvoidPad}>
+                            {container}
+                        </KeyboardAvoid>
+                    }
+                    {!avoidKeyboard &&
+                        container
+                    }
+                </Container>
+                <View style={styles.safeAreaBottom}>
+                    <View style={styles.safeAreaBottomInner} />
+                </View>
+            </SafeAreaView>
+        );
+    }
 }
