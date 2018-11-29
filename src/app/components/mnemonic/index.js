ff556c47496126d9b4a1786c3deb1aba139ec4fc
Ian McGregor <ian@mcgregor.codes>
2018-11-29 17:15:19 +0000

    :iphone: Extend mnemonic text field height for small phone

--- src/app/components/mnemonic/index.js
+++ src/app/components/mnemonic/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Dimensions} from 'react-native';
 import TextInput from '../../components/text-input';
 import styles from './styles';
+
 
 const Word = ({word, selected}) => (
     <View style={[styles.word, selected ? styles.word__selected : null]}>
@@ -13,7 +14,7 @@ const Word = ({word, selected}) => (
 
 const Input = ({value}) => (
     <TextInput
-        numberOfLines={3}
+        numberOfLines={Dimensions.get('window').width < 321 ? 4 : 3}
         value={value}
         onChangeText={() => {}}
         editable={false}
@@ -33,7 +34,7 @@ export default ({
             <View>
                 <Input value={mnemonicConfirm.map(index => words[index]).join(' ')}/>
                 <View style={styles.wordHolder}>
-                    {words.map((word,index) => (
+                    {words.map((word, index) => (
                         <TouchableOpacity key={index} onPress={() => onSelect(index)}>
                             <Word
                                 word={word}
